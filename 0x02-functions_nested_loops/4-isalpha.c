e046d150adb4104f110f704997276364bbc970b6
Lotizzle <lotharmbaegbu@gmail.com>
2023-06-21 21:16:51 -0700

    Another fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
@@ -15,6 +15,6 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
 
