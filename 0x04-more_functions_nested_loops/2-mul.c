84bfc28ea4a66cae58044872457312380957676a
Lotizzle <lotharmbaegbu@gmail.com>
2023-06-23 20:54:37 -0700

    Printing select numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 /**
  * mul - Multiples two integers
- * @: first int
+ * @a: first int
  * @b: second int
  * Return: addition of the multiplication
  */
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
