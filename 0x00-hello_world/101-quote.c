d7706ac95cc0b6ab24c8a85aa1e868b15da22215
Lotizzle <lotharmbaegbu@gmail.com>
2023-06-27 11:42:24 -0700

    Quick fix to the comment section

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
- * 
- * return - 1 (failure)
+ * main - entry point of the program
+ *
+ * Return: 0 on successful execution, non-zero on error
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
