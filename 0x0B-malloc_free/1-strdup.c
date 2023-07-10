c859bd04a845e1ae32f427c6d94034630011e009
Lotizzle <lotharmbaegbu@gmail.com>
2023-07-10 15:24:39 -0700

    printing a duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate
+ * of the string str
+ * @str: pointer to string str
+ *
+ * Return: pointer to the duplicated string, NULL if fails
+ */
+
+char *_strdup(char *str)
+{
+	char *bbb;
+	int i, l;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+
+	bbb = malloc(sizeof(char) * (i + 1));
+
+	if (bbb == NULL)
+		return (NULL);
+	for (l = 0; str[l]; l++)
+		bbb[l] = str[l];
+	return (bbb);
+}
