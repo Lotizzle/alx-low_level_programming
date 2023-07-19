f606e895d194640b0019486b7a7b8a0980e9203b
Lotizzle <lotharmbaegbu@gmail.com>
2023-07-18 18:31:33 -0700

    Making a new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+/**
+ * struct dog - code block
+ * @name: Dog name
+ * @age: Age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
