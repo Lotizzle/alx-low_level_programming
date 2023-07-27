bb574d89338afd33996493125d081a46cb2178c0
Lotizzle <lotharmbaegbu@gmail.com>
2023-07-27 13:53:20 -0700

    Quick fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,10 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - a function that prints all the elements of a list
+ * print_list - a function that prints all the elements of a linked list
  * @h: pointer to the elements of list_t
+ *
  * Return: the number of nodes
  */
 
