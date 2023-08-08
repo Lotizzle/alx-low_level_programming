acbe8571bb60fb394f3494554e169d535ccdd666
Lotizzle <lotharmbaegbu@gmail.com>
2023-08-07 18:36:21 -0700

    Creating a new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: The name of the file to be created
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd, w;
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+
+	w = write(fd, text_content, i);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
