deb2d6affb8c332606cec7873151c1ebd3e25c0f
Lotizzle <lotharmbaegbu@gmail.com>
2023-09-18 17:32:45 -0700

    Calling a dynamic library into a python file

---
+++ 0x18-dynamic_libraries/math_operations.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * add - sums two integers
+ * a: first integer
+ * b: second integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * a: first integer
+ * b: second integer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * a: first integer
+ * b: second integer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides one integer by another
+ * a: first integer
+ * b: second integer
+ */
+int div(int a, int b)
+{
+	return ((float)a / b);
+}
+
+/**
+ * mod - divides one integer by another to find remainder
+ * a: first integer
+ * b: second integer
+ */
+int mod(int a, int b)
+{
+
+	return (a % b);
+}
